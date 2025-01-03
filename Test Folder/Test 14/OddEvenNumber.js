fa93aa44740dd49653f37ef38d170ca91dc601c3
Dhruv Patel <dhruvpatel0201@gmail.com>
2025-01-03 20:21:27 +0530

    ADDED Test 14

---
+++ Test Folder/Test 14/OddEvenNumber.js
@@ -0,0 +1,11 @@
+function checkNumber(num) {
+    if (num % 2 === 0) {
+        console.log(num + " is even");
+    } else {
+        console.log(num + " is odd");
+    }
+}
+
+// Example usage
+checkNumber(4); 
+checkNumber(5); 
